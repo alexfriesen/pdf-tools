26a567cd7f875927b5fb1b2218db808c6b32e4e3
Alexander Friesen <friesen.alex.home@gmail.com>
2023-06-11 16:10:11 +0200

    ♻️ use provideTransloco from transloco

--- src/app/helpers/transloco.helper.ts
+++ src/app/helpers/transloco.helper.ts
@@ -1,44 +1,11 @@
-import {
-  inject,
-  Injectable,
-  isDevMode,
-  makeEnvironmentProviders,
-  Provider,
-  Type,
-} from '@angular/core';
+import { inject, Injectable, isDevMode } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {
-  defaultProviders,
-  TRANSLOCO_CONFIG,
-  TRANSLOCO_LOADER,
   translocoConfig,
-  TranslocoConfig,
   TranslocoLoader,
   Translation,
   getBrowserLang,
 } from '@ngneat/transloco';
-
-type TranslocoOptions = {
-  config?: TranslocoConfig;
-  loader?: Type<TranslocoLoader>;
-};
-
-export function provideTransloco(options: TranslocoOptions) {
-  const providers: Provider[] = [...defaultProviders];
-
-  if (options.config) {
-    providers.push({
-      provide: TRANSLOCO_CONFIG,
-      useValue: options.config,
-    });
-  }
-
-  if (options.loader) {
-    providers.push({ provide: TRANSLOCO_LOADER, useClass: options.loader });
-  }
-
-  return makeEnvironmentProviders(providers);
-}
 
 export function generateAutoConfig(availableLangs: string[]) {
   const browserLang = getBrowserLang();
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,13 @@ import { importProvidersFrom } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
+import { provideTransloco } from '@ngneat/transloco';
 
 import { AppComponent } from './app/app.component';
-
-import { TranslocoHttpLoader, generateAutoConfig, provideTransloco } from './app/helpers/provide-transloco';
+import {
+  TranslocoHttpLoader,
+  generateAutoConfig,
+} from './app/helpers/transloco.helper';
 
 bootstrapApplication(AppComponent, {
   providers: [
